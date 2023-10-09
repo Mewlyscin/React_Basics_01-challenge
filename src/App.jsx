79389448390effb1378f69ec15374d52d841ec87
Mewlyscin <Tear-Lyons7@live.fr>
2023-10-09 19:02:37 +0200

    retire le import Hello (test)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
-import HelloWorld from './components/HelloWorld'
 
 function App() {
   return (
