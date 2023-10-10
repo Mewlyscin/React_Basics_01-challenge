9dd1cf57be9ecf1f41ef48f80330e1a40408e611
Mewlyscin <Tear-Lyons7@live.fr>
2023-10-10 23:40:27 +0200

    ajout compo et modif

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,32 @@
+const pokemonList = [
+  {
+    name: "bulbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1];
+
+  if (pokemon.imgSrc) {
     return (
-        <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-        <figcaption>
-            "Bulbizarre"
-        </figcaption>
-   </figure>
-    )
+      <figure>
+        <img src={pokemon.imgSrc}></img>
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  } else {
+    return (
+      <figure>
+        <p>???</p>
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+  }
 }
 
 export default PokemonCard;
